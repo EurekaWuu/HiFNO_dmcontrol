59a904574eb172bc114d90d26935a20d6a1e4303
Eureka <1318713375@qq.com>
2024-12-19 23:21:52 +0800

    新增hifno_multigpu

--- dmcontrol-generalization-benchmark-main/src/arguments.py
+++ dmcontrol-generalization-benchmark-main/src/arguments.py
@@ -80,7 +80,9 @@ def parse_args():
 
 	args = parser.parse_args()
 
-	assert args.algorithm in {'sac', 'rad', 'curl', 'pad', 'soda', 'drq', 'svea', 'hifno'}, f'specified algorithm "{args.algorithm}" is not supported'
+	assert args.algorithm in {
+		'sac', 'rad', 'curl', 'pad', 'soda', 'drq', 'svea', 'hifno', 'hifno_multigpu'
+	}, f'specified algorithm "{args.algorithm}" is not supported'
 
 	assert args.eval_mode in {'train', 'color_easy', 'color_hard', 'video_easy', 'video_hard', 'distracting_cs', 'none'}, f'specified mode "{args.eval_mode}" is not supported'
 	assert args.seed is not None, 'must provide seed for experiment'
