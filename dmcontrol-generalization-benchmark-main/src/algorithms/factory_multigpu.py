90f758c74a52b2d60d7173801032a5e11ff94ebb
Eureka <1318713375@qq.com>
2024-12-20 11:23:04 +0800

    区分multigpu

--- dmcontrol-generalization-benchmark-main/src/algorithms/factory_multigpu.py
+++ dmcontrol-generalization-benchmark-main/src/algorithms/factory_multigpu.py
@@ -1,3 +1,4 @@
+import torch
 from algorithms.sac import SAC
 from algorithms.rad import RAD
 from algorithms.curl import CURL
@@ -6,6 +7,7 @@ from algorithms.soda import SODA
 from algorithms.drq import DrQ
 from algorithms.svea import SVEA
 from algorithms.hifno import HiFNOAgent
+from algorithms.hifno_multigpu import HiFNOAgent as HiFNOAgentMultiGPU
 from algorithms.svea_vis import SVEA_VIS
 
 algorithm = {
@@ -17,15 +19,20 @@ algorithm = {
 	'drq': DrQ,
 	'svea': SVEA,
 	'hifno': HiFNOAgent,
+	'hifno_multigpu': HiFNOAgentMultiGPU,
     'svea_vis': SVEA_VIS
 }
 
 
 def make_agent(obs_shape, action_shape, args):
-	if args.algorithm == 'hifno':
-		return HiFNOAgent(
+	if args.algorithm in ['hifno', 'hifno_multigpu']:
+		agent_class = algorithm[args.algorithm]
+		agent = agent_class(
 			obs_shape=obs_shape,
 			action_shape=action_shape,
 			args=args
 		)
+		agent = agent.to(torch.device('cuda:0'))
+		return agent
+	# 对于其他算法
 	return algorithm[args.algorithm](obs_shape, action_shape, args)
