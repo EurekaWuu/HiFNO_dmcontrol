ef0dbc1529d094065d9391744e0a5ccf6f26602b
Eureka <1318713375@qq.com>
2025-03-11 09:58:18 +0800

    不再手动计算nproc_per_node

--- dmcontrol-generalization-benchmark-main/src/run_hifno_aug.sh
+++ dmcontrol-generalization-benchmark-main/src/run_hifno_aug.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # ./run_hifno_aug.sh
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+export CUDA_VISIBLE_DEVICES=0,6,7
 
 export TORCHELASTIC_ERROR_FILE="/tmp/torch_elastic_error.json"
 
+GPU_COUNT=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+
+LOG_DIR="/mnt/lustre/GPU4/home/wuhanpeng/dmcontrol/src/logs/walker_walk/hifno_multigpu_aug"
+
 torchrun \
-    --nproc_per_node=6 \
+    --nproc_per_node=$GPU_COUNT \
     --master_port=29500 \
     train_hifno_aug.py \
     --algorithm hifno_multigpu \
@@ -15,8 +19,9 @@ torchrun \
     --task_name walk \
     --seed 1 \
     --lr 1e-4 \
-    --embed_dim 196 \
+    --embed_dim 144 \
     --batch_size 24 \
     --num_scales 3 \
     --depth 3 \
-    --patch_size 4
+    --patch_size 4 \
+    --log_dir ${LOG_DIR}
